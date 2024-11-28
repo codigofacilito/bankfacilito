fdff01712c10967f8fc8ccf144836c152994164e
eduardogpg <eduardo78d@gmail.com>
2024-11-27 22:43:24 -0600

    Past unitest over users

--- spec/factories/accounts.rb
+++ spec/factories/accounts.rb
@@ -15,8 +15,7 @@ FactoryBot.define do
   factory :account do
     user
     account_number { "1234567890" }
-    account_type   { 1 }
     balance        { 1_000 }
-    CLABE          { "123456789012345678" }
+    clabe          { "123456789012345678" }
   end
 end
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -11,56 +11,50 @@ RSpec.describe 'Users API', type: :request do
       }
     end
 
+    let(:endpoint) { '/api/v1/register' }
+
     context 'when the request is valid' do
-      it 'creates a new user' do
+      it 'Create a new User' do
         expect {
-          post '/register', params: valid_attributes
+          post endpoint, params: valid_attributes
         }.to change(User, :count).by(1)
       end
 
-      it 'creates an associated account with an initial balance of $1,000' do
-        post '/register', params: valid_attributes
-        user = User.last
-        account = user.accounts.first
-        expect(account).not_to be_nil
-        expect(account.balance).to eq(1_000)
+      it 'returns status code 201' do
+        post endpoint, params: valid_attributes
+        expect(response).to have_http_status(201)
       end
+
     end
 
-    context 'when the request is invalid' do
-      it 'does not create a user without email' do
-        invalid_attributes = valid_attributes.except(:email)
-        expect {
-          post '/register', params: invalid_attributes
-        }.not_to change(User, :count)
-      end
-    end
   end
 
   describe 'POST /login' do
     let(:user) { create(:user, pin: '1234') }
     let(:account) { create(:account, user: user) }
-
+    let(:endpoint) { '/api/v1/login' }
+  
     context 'with valid credentials' do
       it 'logs in successfully' do
-        post '/login', params: { account_number: account.account_number, pin: '1234' }
+        post  endpoint, params: { account_number: account.account_number, pin: '1234' }
         expect(response).to have_http_status(:ok)
         expect(JSON.parse(response.body)['message']).to eq('Inicio de sesión exitoso')
       end
     end
-
+  
     context 'with invalid credentials' do
       it 'rejects login with incorrect PIN' do
-        post '/login', params: { account_number: account.account_number, pin: '0000' }
+        post  endpoint, params: { account_number: account.account_number, pin: '0000' }
         expect(response).to have_http_status(:unauthorized)
         expect(JSON.parse(response.body)['errors']).to eq('Número de cuenta o PIN incorrecto')
       end
-
+  
       it 'rejects login with incorrect account number' do
-        post '/login', params: { account_number: 'invalid', pin: '1234' }
+        post  endpoint, params: { account_number: 'invalid', pin: '1234' }
         expect(response).to have_http_status(:unauthorized)
         expect(JSON.parse(response.body)['errors']).to eq('Número de cuenta o PIN incorrecto')
       end
     end
   end
+  
 end
