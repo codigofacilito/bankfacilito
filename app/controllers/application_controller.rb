f3df0eb2be5bf7cdb2ff04a3af4154cbaf47f088
Claudia Gonzalez <claunicole.gf@gmail.com>
2024-12-02 22:52:05 -0300

    feat: add jwt to users and application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,26 @@
 class ApplicationController < ActionController::API
+  before_action :authenticate_request
+
+  attr_reader :current_user
+
+  private
+
+  def authenticate_request
+    header = request.headers['Authorization']
+    if header.present?
+      token = header.split(' ').last
+      begin
+        decoded = JsonWebToken.decode(token)
+        @current_user = User.find(decoded['user_id'])
+      rescue ActiveRecord::RecordNotFound => e
+        render json: { errors: 'Usuario no encontrado' }, status: :unauthorized
+      rescue JWT::ExpiredSignature
+        render json: { errors: 'Token expirado' }, status: :unauthorized
+      rescue JWT::DecodeError
+        render json: { errors: 'Token inválido' }, status: :unauthorized
+      end
+    else
+      render json: { errors: 'Token no proporcionado' }, status: :unauthorized
+    end
+  end
 end
