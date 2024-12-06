2c6d1f500c38b3925724ad189bd13495eb8e6018
Claudia Gonzalez <claunicole.gf@gmail.com>
2024-12-06 15:17:48 -0300

    fix: add skip before action

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,21 +6,21 @@ class ApplicationController < ActionController::API
   private
 
   def authenticate_request
-    # header = request.headers['Authorization']
-    # if header.present?
-    #   token = header.split(' ').last
-    #   begin
-    #     decoded = JsonWebToken.decode(token)
-    #     @current_user = User.find(decoded['user_id'])
-    #   rescue ActiveRecord::RecordNotFound => e
-    #     render json: { errors: 'Usuario no encontrado' }, status: :unauthorized
-    #   rescue JWT::ExpiredSignature
-    #     render json: { errors: 'Token expirado' }, status: :unauthorized
-    #   rescue JWT::DecodeError
-    #     render json: { errors: 'Token inválido' }, status: :unauthorized
-    #   end
-    # else
-    #   render json: { errors: 'Token no proporcionado' }, status: :unauthorized
-    # end
+     header = request.headers['Authorization']
+     if header.present?
+       token = header.split(' ').last
+       begin
+         decoded = JsonWebToken.decode(token)
+         @current_user = User.find(decoded['user_id'])
+       rescue ActiveRecord::RecordNotFound => e
+         render json: { errors: 'Usuario no encontrado' }, status: :unauthorized
+       rescue JWT::ExpiredSignature
+         render json: { errors: 'Token expirado' }, status: :unauthorized
+       rescue JWT::DecodeError
+         render json: { errors: 'Token inválido' }, status: :unauthorized
+       end
+     else
+       render json: { errors: 'Token no proporcionado' }, status: :unauthorized
+     end
   end
 end
